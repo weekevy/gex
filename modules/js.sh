83a23b241a2aae3bfe9c62ee167116671f033f4e
dvsys <dvsys@localhost.localdomain>
2026-01-21 12:43:46 +0100

    edit js.sh module
    make theme more better, fix  --content argument options, -f (get content list of js endpoints) -u (get content of single js endpoint) -o (output)

--- modules/js.sh
+++ modules/js.sh
@@ -12,7 +12,7 @@ WHITE='\033[0;37m'
 BOLD='\033[1m'
 UNDERLINE='\033[4m'
 RESET='\033[0m'
-cd "$(dirname "$0")"
+ORIG_DIR="$(pwd)"
 #
 #
 #
@@ -23,41 +23,60 @@ getQuery () {
 
 }
 
-
-
-scanRegex_file() {
-    local target_file="$1"
-    local total_matches=0
-
-    local API_REGEX="$(dirname "${BASH_SOURCE[0]}")/../regex/api_regex.json"
-
-    echo "[*] Scanning file: $target_file"
-    echo ""
-
-    local keys
-    keys=$(jq -r 'keys[]' "$API_REGEX")
-
-    for key in $keys; do
-        local regex
-        regex=$(jq -r --arg k "$key" '.[$k]' "$API_REGEX")
-
-        local matches
-        matches=$(grep -oP "$regex" "$target_file" | sort -u)
-
-        if [[ -n "$matches" ]]; then
-            local count
-            count=$(echo "$matches" | wc -l)
-
-            echo "[+] Key: $key"
-            echo "$matches"
-            echo ""
-
-            total_matches=$((total_matches + count))
-        fi
+getQuery_dir() {
+    local target_dir="$1"
+    echo "[*] Scanning directory for juicy information: $target_dir"
+    find "$target_dir" -type f -print0 | while IFS= read -r -d $'\0' file; do
+        getQuery "$file"
     done
+}
 
-    echo "[*] Total matches found: $total_matches"
-    echo "[*] Scan complete."
+getContent () {
+    local url=$1
+    local output_file=$2
+    if [ -n "$output_file" ]; then
+        curl -s "$url" | tee -a "$output_file"
+    else
+        curl -s "$url"
+    fi
+}
+
+process_js_file () {
+    local file="$1"
+    local output_file=$2
+    if [[ "$file" != /* ]];then
+        file="$ORIG_DIR/$file"
+    fi
+
+    file="${file//$'\r'/}"
+
+    if [[ ! -f "$file" ]]; then
+        echo "file not found :$file"
+        exit 1
+    fi
+
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        [[ -z "$line" ]] && continue
+        if [[ "$line" =~ ^https?://.+\.js$ ]]; then
+            if [ -n "$output_file" ]; then
+                curl -s "$line" | tee -a "$output_file"
+            else
+                curl -s "$line"
+            fi
+        else
+            if [ -n "$output_file" ]; then
+                echo "Invalid JS URL :$line" | tee -a "$output_file"
+            else
+                echo "Invalid JS URL :$line"
+            fi
+        fi
+    done < "$file"
+}
+
+
+getJuicyinfo (){
+
+    local fileToExtract=$1
 }
 
 
@@ -66,19 +85,22 @@ scanRegex_file() {
 
 
 
+
+
 if [[ $# -eq 0 || "$1" == "--help" ]]; then
-  echo -e "${YELLOW}  Usage: ./quevy regex [--module] <target> ${RESET}"
-    
-
-  echo -e "${MAGENTA}  [options]${RESET}"
-  echo "        --secrets     check for existed regex in file" 
-  echo "        --juicy       Can contain juicy information [md5, sha, other encryption types]"
-
-  echo -e "${MAGENTA}  [target]${RESET}"
-  echo -e "         -url <url>        JavaScript endpoint extract"
-  echo -e "         -f   <file>       file contain Java script | List of Java script endpoints"
-  echo -e "         -d   <directory>  Search directory recursively"  
-
+  echo -e "${YELLOW}Usage: ./gex js [--module] [options] <target>${RESET}"
+  echo ""
+  echo -e "${MAGENTA}[modules]${RESET}"
+  echo "  --secrets     Check for secrets in files/directories."
+  echo "  --juicy       Find juicy information in files/directories."
+  echo "  --content     Get content from a URL."
+  echo ""
+  echo -e "${MAGENTA}[options]${RESET}"
+  echo "  -u <url>        Url target"
+  echo "  -f <file>       Input file."
+  echo "  -d <directory>  Input directory."
+  echo "  -o <output>     Write output to a file."
+  echo ""
   exit 0
 fi
 
@@ -88,118 +110,156 @@ fi
 
 
 case "$1" in
-    --secrets)
-        echo "You are using [js] module"
+        # --secrets)
+        #     echo "You are using [js] module"
+        #     shift
+        #     local secrets_target=""
+        #     local output_file=""
+    
+        #     while [[ $# -gt 0 ]]; do
+        #         case "$1" in
+        #             -f)
+        #                 secrets_target="$2"
+        #                 shift 2
+        #                 ;;
+        #             -d)
+        #                 secrets_target="$2"
+        #                 shift 2
+        #                 ;;
+        #             -o)
+        #                 output_file="$2"
+        #                 shift 2
+        #                 ;;
+        #             *)
+        #                 echo "Unknown option for --secrets: $1"
+        #                 exit 1
+        #                 ;;
+        #         esac
+        #     done
+    
+        #     if [ -n "$output_file" ]; then
+        #         exec > "$output_file"
+        #     fi
+    
+        #     if [ -f "$secrets_target" ]; then
+        #         scanRegex_file "$secrets_target"
+        #     elif [ -d "$secrets_target" ]; then
+        #         if [[ "$secrets_target" == "." ]]; then
+        #             scanRegex_dir "$(pwd)"
+        #         else
+        #             scanRegex_dir "$secrets_target"
+        #         fi
+        #     else
+        #         echo "Error: there is no file or directory"
+        #         exit 1
+        #     fi
+        #
+        
+    # --juicy)
+    #     shift
+    #     juicy_target=""
+    #     output_file=""
+
+    #     while [[ $# -gt 0 ]]; do
+    #         case "$1" in
+    #             -f)
+    #                 juicy_target="$2"
+    #                 shift 2
+    #                 ;;
+    #             -d)
+    #                 juicy_target="$2"
+    #                 shift 2
+    #                 ;;
+    #             -o)
+    #                 output_file="$2"
+    #                 shift 2
+    #                 ;;
+    #             *)
+    #                 echo "Unknown option for --juicy: $1"
+    #                 exit 1
+    #                 ;;
+    #         esac
+    #     done
+
+    #     if [ -n "$output_file" ]; then
+    #         exec > "$output_file"
+    #     fi
+
+    #     if [ -f "$juicy_target" ]; then
+    #         getQuery "$juicy_target"
+    #     elif [ -d "$juicy_target" ]; then
+    #         getQuery_dir "$juicy_target"
+    #     else
+    #         echo "Error: there is no file or directory"
+    #         exit 1
+    #     fi
+    #     ;;
+
+
+
+    --content)
         shift
-        case "$1" in
-            -f)
-                if [ ! -f "$2" ]; then
-                    echo "Error: there is no file"
+        content_url=""
+        output_file=""
+        content_file=""
+        
+        while [[ $# -gt 0 ]]; do
+            case "$1" in
+                -u)
+                    if [[ -z "$2" || "$2" == -* ]];then
+                        echo "Invalid URL: -u requires a value"
+                        exit 1
+                    fi
+                    content_url="$2"
+                    shift 2
+                    ;;
+                -f|-fil)
+                    if [[ -z "$2" || "$2" == -* ]];then
+                        echo "Invalid file: -f requires a value"
+                        exit 1
+                    fi
+                    content_file="$2"
+                    shift 2
+                    ;;
+
+                -o)
+                    output_file="$2"
+                    shift 2
+                    ;;
+                *)
+                    echo "Unknown option for --content: $1"
                     exit 1
-                fi
-                FILE_PATH="$2"
-                # /////////////////////////////////// 
-                scanRegex_file "$FILE_PATH"
-                # /////////////////////////////////// 
-                shift 2 
-                ;;
-            -d)
-                if [ ! -d "$2" ]; then
-                    echo "Error: there is not directory"
-                    exit 1
-                fi
-                DIRECTORY_PATH="$2"
-                # /////////////////////////////////// 
-                if [[ "$DIRECTORY_PATH" == "." ]]; then
-                    scanRegex_dir "$(pwd)"
+                    ;;
+            esac
+        done
 
-                fi
-                scanRegex_dir "$DIRECTORY_PATH"                                
-                
-                # /////////////////////////////////// 
-                shift 2
-                ;;
-            
-            -url)
-                URL=$2
-                if [[ $URL =~ ^https?://.*\.js$ ]]; then
-                # /////////////////////////////////// 
+        # cannot use both
+        if [[ -n "$content_url" && -n "$content_file" ]];then
+            echo "Use either -u OR -f, not both"
+            exit 1
+        fi
 
 
-                    python3 ../pylib/getContent.py -u $URL > tmp_file                   
-                    scanRegex_file tmp_file
-                    rm  tmp_file
-                    
-
-
-                # /////////////////////////////////// 
-                else
-                    echo "Wrong url form !"
-                fi
-                shift 2
-                ;;
-            *)
-                echo "no target "
-                exit 1 
-                ;;
-        esac
-        ;;
-    --juicy)
-        echo "You are using PARAM module"
-        shift
-        case "$1" in
-            -f)
-                if [ ! -f "$2" ]; then
-                    echo "Error: there is no file"
-                    exit 1
-                fi
-                FILE_PATH="$2"
-                # /////////////////////////////////// 
-                getQuery "$FILE_PATH"                
-                # /////////////////////////////////// 
-                shift 2 
-                ;;
-            -d)
-                if [ ! -d "$2" ]; then
-                    echo "Error: there is no directory"
-                    exit 1
-                fi
-                DIRECTORY_PATH="$2"
-                if [ "$DIRECTORY_PATH" ]]; then
-                    echo "Hello"
-                fi
-                # do some shit over here
-                shift 2
-                ;;
-            *)
-                echo "no target "
-                exit 1 
-                ;;
-        esac
+        # single JS URL
+        if [[ -n "$content_url" ]]; then
+            if [[ "$content_url" =~ ^https?://.+\.js$ ]]; then
+                getContent "$content_url" "$output_file"
+            else
+                echo "Invalid URL"
+                exit 1
+            fi
+        elif [[ -n "$content_file" ]]; then
+            process_js_file "$content_file" "$output_file"
+        else
+            echo "Invalid Input: use -u or -f"
+            exit 1
+        fi
         ;;
     *)
         echo "No Module with this name"
         exit 1
         ;;
 esac
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
 
 
 
